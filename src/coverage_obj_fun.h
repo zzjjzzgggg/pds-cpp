df91bbe4a508da4bdf62349526c35948f2ac8c28
J. Zhao <zzjjzzgggg@gmail.com>
2019-03-25 16:52:59 +0300

    add objective function

---
+++ src/coverage_obj_fun.h
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) by J.Z. 2019-03-25 16:39
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef __COVERAGE_OBJ_FUN_H__
+#define __COVERAGE_OBJ_FUN_H__
+
+#include "obj_fun.h"
+
+class CoverageObjFun : public ObjFun {
+public:
+    CoverageObjFun() : ObjFun() {}
+
+}; /*  */
+
+#endif /* __COVERAGE_OBJ_FUN_H__ */
---
+++ src/obj_fun.h
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C) by J.Z. 2019-03-25 16:37
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef __OBJ_FUN_H__
+#define __OBJ_FUN_H__
+
+#include "stdafx.h"
+
+class ObjFun {
+public:
+    virtual ~ObjFun() {}
+
+    virtual double getVal(const int) const = 0;
+    virtual double getVal(const std::vector<int>&) const = 0;
+    virtual double getVal(const std::unordered_set<int>&) const = 0;
+    virtual double getGain(const int, const std::vector<int>&) const = 0;
+    virtual double getGain(const int, const std::unordered_set<int>&) const = 0;
+
+}; /* ObjFun */
+
+#endif /* __OBJ_FUN_H__ */
